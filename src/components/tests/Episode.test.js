bb33a0572e859c63814e6a0c809450f06f576bb1
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-07-07 12:45:51 -0400

    Completed Episode.test.js

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,24 +8,55 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "This movie is good",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "This movie is good",
+    runtime: 1
 }
 
 test("renders without error", () => {
+    // Arrange
+    const episode = render(<Episode episode={testEpisode}/>);
 
+    // Act
+
+    // Assert
+    expect(episode).toBeTruthy();
+    
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summary test passed as prop", ()=>{
+      // Arrange
+      render(<Episode episode={testEpisode}/>);
+      const summary = screen.getByText(/this movie is good/i)
+  
+      // Act
+  
+      // Assert
+      expect(summary).toBeInTheDocument();
+      expect(summary).toBeTruthy();
+      expect(summary).toBeVisible();
+  
+
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    // Arrange
+    render(<Episode episode={testEpisodeWithoutImage}/>);
+    const blankImage = screen.getByAltText( './stranger_things.png')
+    // Act
+
+    // Assert 
+    expect(blankImage).toBeInTheDocument();
 })
 
 //Tasks
